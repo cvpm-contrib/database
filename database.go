f56a5a713f7e89b358e3cd58a75e968dd7a97a9a
Xiaozhe Yao <407718364@qq.com>
2019-02-01 18:47:26 +0800

    init repo

---
+++ database.go
@@ -0,0 +1,50 @@
+package database
+
+import (
+	"database/sql"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+	"upper.io/db.v3/lib/sqlbuilder"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"upper.io/db.v3/sqlite"
+)
+
+var settings = sqlite.ConnectionURL{
+	Database: filepath.Join(getDBDir(), "cvpm-database.db"), // Path to a sqlite3 database file.
+}
+
+func initDatabase() {
+	database, err := sql.Open("sqlite3", filepath.Join(getDBDir(), "cvpm-database.db"))
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS dataset (id INTEGER PRIMARY KEY, Name TEXT, Desc TEXT, Tags TEXT, Files TEXT, Link TEXT)")
+	if err != nil {
+		panic(err)
+	}
+	statement.Exec()
+	database.Close()
+}
+
+func getDBDir() string {
+	homePath, _ := homedir.Dir()
+	cvpmPath := filepath.Join(homePath, "cvpm")
+	dbPath := filepath.Join(cvpmPath, "database")
+	return dbPath
+}
+
+func GetDBInstance() sqlbuilder.Database {
+	initDatabase()
+	sess, err := sqlite.Open(settings)
+	if err != nil {
+		panic(err)
+	}
+	return sess
+}
+
+func CloseDB(sess sqlbuilder.Database) {
+	defer sess.Close()
+}
+
+func initDB() {
+
+}
